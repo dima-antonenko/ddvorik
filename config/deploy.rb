b5729841cf8baa2edb911f797c5da0387e4dcfa9
dima-antonenko <d.antonenko94@mail.ru>
2015-05-07 11:14:15 +0300

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -103,6 +103,17 @@ namespace :deploy do
 
   after :finishing, 'deploy:cleanup'
   
+  desc 'copy ckeditor nondigest assets'
+task :copy_nondigest_assets do
+  on roles :app do
+    within fetch(:release_path) do
+      with rails_env: fetch(:rails_env) do
+        execute :rake, "ckeditor:create_nondigest_assets"
+      end
+    end
+  end
+end
+after 'deploy:assets:precompile', 'copy_nondigest_assets'
 
 end
 
