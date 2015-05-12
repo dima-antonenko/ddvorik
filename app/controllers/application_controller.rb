2172fe8a5e59bc5a6c31325ab75cbdd71370c3f6
dima-antonenko <d.antonenko94@mail.ru>
2015-05-12 03:16:19 +0300

    updates

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -12,11 +12,11 @@ class ApplicationController < ActionController::Base
 
 
   def after_sign_in_path_for(resource)
-  	'/administrator/dashboard'
+    '/administrator/dashboard'
   end
 
   def after_sign_out_path_for(resource_or_scope)
-  	 request.referrer
+     request.referrer
   end
 
   def minicart_in_head
